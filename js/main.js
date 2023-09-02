a1a27ace9be214220f4a7605d08105ffa9631c4b
abdulazizthemodernworker@gmail.com <abdulazizthemodernworker@gmail.com>
2023-09-02 22:46:37 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let loginBlock = document.getElementById('login');
+let homeBlock = document.getElementById('home');
+let username = document.getElementById('username');
+let login = false;
+let home = false;
+
+let user = 'admin';
+
+if (!login) {
+  loginBlock.style.display = 'none';
+} else {
+  loginBlock.style.display = 'block';
+}
+
+if (!home) {
+  homeBlock.style.display = 'none';
+}
+
+function loginf() {
+  login = true
+  loginBlock.style.display = 'block';
+}
+
+function loginUser() {
+  if (username.value === user) {
+    home = true;
+    loginBlock.style.display = 'none';
+    homeBlock.style.display = 'block'
+  } else {
+    alert('testdan mufaqyatli ottingiz')
+  }
+}
